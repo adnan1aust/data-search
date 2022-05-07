6d4f5cbf047546469733735512b89cb09548fbbc
Ahmed Shahriar Adnan <adnan1aust@users.noreply.github.com>
2022-05-07 12:50:42 +0600

    Search box asperated

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-import  { Component } from 'react';
+import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component{
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. Testing
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			monsters: [],
+			searchTerm: ''
+		}
+	}
+
+	render() {
+		const { monsters, searchTerm } = this.state;
+		const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchTerm.toLowerCase()));
+		return (
+			<div className="App">
+				<SearchBox placeholder='Search Monsters'
+				handleChange={e => this.setState({ searchTerm: e.target.value })}/>
+				<CardList monsters = { filteredMonsters }/>
+			</div>
+		);
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({ monsters: users }));
+	}
 }
 
 export default App;
